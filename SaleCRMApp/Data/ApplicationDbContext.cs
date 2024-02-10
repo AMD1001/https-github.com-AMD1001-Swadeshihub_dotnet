90be0582573102f7d71acd6979664f90bfe5afc3
Ajinkya Dhumal <mdhumal68@gmail.com>
2024-02-10 13:30:26 +0530

    Add project files.

---
+++ SaleCRMApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SaleCRMApp.Models;
+using SwadeshiApp.DTO;
+using SwadeshiApp.Models;
+
+namespace SaleCRMApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<UserEntity> user { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Review> Review { get; set; }
+        public DbSet<Wishlist> Wishlist { get; set; }
+        public DbSet<Supplier> Supplier { get; set; }
+        public DbSet<State> States { get; set; }
+
+        public DbSet<UserProfile> users { get; set; }
+    }
+}
